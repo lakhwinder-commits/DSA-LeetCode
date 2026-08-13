3e6e3d00f1068d7a269aa0e524ba7192179ecc6c
Lakhwinder Singh <lakhwinder32105@gmail.com>
2026-08-13 14:42:21 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (24.98%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -1,19 +1,23 @@
 class NumArray {
     vector<int> nums;
+    vector<int> prefix;
 
 public:
     NumArray(vector<int>& nums) {
         this->nums = nums;
+        prefix.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            prefix.push_back(nums[i]+prefix[i-1]);
+        }
     }
+    
 
     int sumRange(int left, int right) {
-        int sum = 0;
+        
 
-        while (left <= right) {
-            sum += nums[left];
-            left++;
-        }
+        if (left == 0)
+        return prefix[right];
 
-        return sum;
+    return prefix[right] - prefix[left - 1];
     }
 };
